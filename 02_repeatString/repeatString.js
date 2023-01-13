12aabac7e7a729b0e7fc39ca4fd44397bb537036
Danny Simms <djfsimms@hotmail.com>
2023-01-12 22:06:59 -0330

    Add repeatString solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    const userString = string;
+    let stringRepeated = "";
+    const repeat = num;
 
+    if (repeat >= 0) {
+        for (i = 0; i < repeat; i++) {
+            stringRepeated += userString;
+        }
+    } else {
+        stringRepeated = "ERROR";
+    }
+
+    return(stringRepeated);
 };
 
 // Do not edit below this line
