1bd2a4f5eff3f30d85b32fbd83ebfb9459e2ab43
Danny Simms <djfsimms@hotmail.com>
2023-01-14 17:16:27 -0330

    Add removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...theArgs) {
+    let myArray = array;
+    const myArgs = theArgs;
+    console.log(theArgs);
+    //given an array
+    //given one or more arguments
+    //for each argument
+    //search the array to see if the argument is inside it
+    //if so, remove that item from the array
+    //if not, don't change the array
+    for (let arg of myArgs) {
+        const index = myArray.indexOf(arg);
 
+        if (index > -1) {
+            myArray.splice(index, 1);
+        }
+    }
+
+    return(myArray);
 };
 
 // Do not edit below this line
