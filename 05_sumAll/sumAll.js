34891d370f83c0a964651a23802cd30a9b85b8c6
Danny Simms <djfsimms@hotmail.com>
2023-01-14 18:19:24 -0330

    Add sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    const startNum = num1;
+    const endNum = num2;
+    let result = 0;
 
+    if (startNum < 0 || endNum < 0) {
+        result = "ERROR";
+    } else if (typeof startNum != "number" || typeof endNum != "number") {
+            result = "ERROR";   
+    } else if (startNum < endNum) {
+            for (let i = startNum; i <= endNum; i++) {
+                result += i;
+            }
+    } else if (startNum > endNum) {
+        for (let i = startNum; i >= endNum; i--) {
+            result += i;
+        }
+    }
+
+    return (result);
 };
 
 // Do not edit below this line
