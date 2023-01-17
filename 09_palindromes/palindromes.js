20582726a0a33ae1a0f41b7e9d0fc521af1c472c
Danny Simms <djfsimms@hotmail.com>
2023-01-16 22:26:28 -0330

    Add solution for palindrones function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,27 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const punctuationlessString = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    const origStringArray = punctuationlessString.split(" ");
+    const testString = origStringArray.join("").toLowerCase();
+    let revString = reverseString(string).toLowerCase();
 
+    if (testString === revString) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+const reverseString = function (string) {
+    // Removes special characters, then white spaces from the string and makes it lower case
+    const noPunctuationString = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replace(/\s+/g, "").toLowerCase();
+    const stringArray = noPunctuationString.split("");
+    let reverseText = "";
+
+    for (i = stringArray.length - 1; i >= 0; i--) {
+        reverseText += stringArray[i];
+    }
+
+    return(reverseText);
 };
 
 // Do not edit below this line
